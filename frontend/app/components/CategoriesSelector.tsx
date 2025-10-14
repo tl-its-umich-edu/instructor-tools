1b2655eee858f2cc7b68ec5cdce14fe08c7de609
Jaydon Krooss <jkrooss@umich.edu>
2025-10-14 11:25:14 -0400

    add checkbox

--- frontend/app/components/CategoriesSelector.tsx
+++ frontend/app/components/CategoriesSelector.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { getCategories } from '../api';
 import React from 'react';
-import { Box, Chip, CircularProgress, FormControl, Grid, InputLabel, MenuItem, OutlinedInput, Select, SelectChangeEvent } from '@mui/material';
+import { Box, Checkbox, Chip, CircularProgress, FormControl, Grid, InputLabel, ListItemText, MenuItem, OutlinedInput, Select, SelectChangeEvent } from '@mui/material';
 import ErrorsDisplay from './ErrorsDisplay';
 import { styled } from '@mui/material/styles';
 
@@ -44,7 +44,7 @@ const MenuProps = {
 };
 
 const CategoriesSelector = ({ categoryIdsSelected, onCategoryIdsSelectedChange }: CategoriesSelectorProps) => {
-  const { data: categories, isLoading: getCategoriesLoading, error: getCategoriesError } = useQuery({
+  const { data: categories = [], isLoading: getCategoriesLoading, error: getCategoriesError } = useQuery<{ id: number; category_name: string }[]>({
     queryKey: ['getCategories'],
     queryFn: getCategories,
   });
@@ -66,57 +66,62 @@ const CategoriesSelector = ({ categoryIdsSelected, onCategoryIdsSelectedChange }
         direction='column'
         alignItems='center'>
         { getCategoriesLoading && 
-        <div aria-describedby='categories-loading' aria-busy={getCategoriesLoading}>
-          <CircularProgress />
-        </div>}
+          <CircularProgress id='categories-loading' />
+        }
         { errors.length > 0 && 
         <Box sx={{ marginBottom: 1 }}>
           <ErrorsDisplay errors={errors} />
         </Box>
         }
         {
-          <FormControl>
-            <InputLabel id="category-select-label">Categories Selected</InputLabel>
-            <Select
-              labelId="category-select-label"
-              id="category-select"
-              className={classes.select}
-              multiple
-              value={categoryIdsSelected}
-              onChange={handleChange}
-              input={<OutlinedInput id="select-multiple-categories" label="Select Categories" />}
-              renderValue={(selectedIds) => (
-                <Box className={classes.box}>
-                  {
-                    categories?.map(
-                      (category) => (
-                        <>
-                          {selectedIds.includes(category.id) && (
-                            <Chip 
-                              key={category.id} 
-                              className={classes.chip}
-                              label={category.category_name}/>
-                          )}
-                        </>
-                      )
-                    )}
-                </Box>
-              )}
-              MenuProps={MenuProps}
-            >
-              {categories?.map((category) => (
-                <MenuItem
-                  key={category.id}
-                  value={category.id}
-                >
-                  {category.category_name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+          <div aria-describedby='categories-loading' aria-busy={getCategoriesLoading}>
+            { !getCategoriesLoading && 
+            <FormControl>
+              <InputLabel id="category-select-label">Categories Selected</InputLabel>
+              <Select
+                labelId="category-select-label"
+                id="category-select"
+                className={classes.select}
+                multiple
+                value={categoryIdsSelected}
+                onChange={handleChange}
+                input={<OutlinedInput id="select-multiple-categories" label="Select Categories" />}
+                renderValue={(selectedIds) => (
+                  <Box className={classes.box}>
+                    {
+                      categories.map(
+                        (category) => (
+                          <>
+                            {selectedIds.includes(category.id) && (
+                              <Chip 
+                                key={category.id} 
+                                className={classes.chip}
+                                label={category.category_name}/>
+                            )}
+                          </>
+                        )
+                      )}
+                  </Box>
+                )}
+                MenuProps={MenuProps}
+              >
+                {categories.map((category) => (
+                  <MenuItem
+                    key={category.id}
+                    value={category.id}
+                  >
+                    <Checkbox checked={categoryIdsSelected.includes(category.id)} />
+                    <ListItemText primary={category.category_name} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            }
+          </div>
         }
       </Grid>
     </SelectorContainer>
+    
   );
 };
 
