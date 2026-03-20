467ff05890766a7a4224e26f7013f2abefdf0076
Pushyami Gundala <pushyamig@users.noreply.github.com>
2026-03-20 16:46:10 -0400

    * #i612 handling multiple Tab session

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from '@mui/material';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { Globals } from './interfaces';
+import { SIGNED_PAYLOAD_STORAGE_KEY } from './constants';
 import theme from './theme';
 import { ConsentManagerProvider } from './components/ConsentManagerProvider';
 import App from './App';
@@ -14,6 +15,12 @@ const globalsEl = document.getElementById(globalsId);
 if (globalsEl === null) throw Error(`"${globalsId}" was not found!`);
 if (globalsEl.textContent === null) throw Error(`No text content in "${globalsId}"!`);
 const globals: Globals = Object.freeze(JSON.parse(globalsEl.textContent));
+
+if (globals.signed_course_user_payload !== null) {
+  sessionStorage.setItem(SIGNED_PAYLOAD_STORAGE_KEY, globals.signed_course_user_payload);
+}else {
+  throw Error('signed_course_user_payload is null in globals!');
+}
 
 const queryClient = new QueryClient({
   defaultOptions: {
