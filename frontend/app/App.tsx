ec5dea01aabe222bb17d9dea47d53f9662c81eda
jaydonkrooss <147423601+jaydonkrooss@users.noreply.github.com>
2026-04-14 15:30:05 -0400

    #567 add Exit Dialog for Review page, back/refresh listeners (#641)
    * #567: add Exit Dialog for Review page, back/refresh listeners
    
    * dont show dialog on review submission
    
    * dirty fields are "approve" by default
    
    * rewrite with useBlocker, useBeforeUnload
    
    * * #i612 handling multiple Tab session
    
    * review suggestions, add onSubmitComplete to bypass exit guard
    
    * remove lti skill until I can make a better one...
    
    * UI review: clarify dialog language
    
    ---------
    
    Co-authored-by: Pushyami Gundala <pushyamig@users.noreply.github.com>

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -1,28 +1,44 @@
 import React from 'react';
 import { Globals } from './interfaces';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, Routes, Outlet } from 'react-router-dom';
 import ToolsHome from './components/ToolsHome';
 import AltTextHome from './components/AltTextHome';
+import HeaderAppBar from './components/HeaderAppBar';
+import AltTextReview from './components/AltTextReview';
 
 
 interface AppProps {
   globals: Globals
 }
 
+function AltTextLayout({ globals }: { globals: Globals }) {
+  return (
+    <>
+      <HeaderAppBar
+        breadcrumbTitle='Alt Text Helper'
+        user={globals.user}
+        helpURL={globals.help_url}
+      />
+      <Outlet />
+    </>
+  );
+}
+
 function App (props: AppProps) {
 
   return (
     <div id='root'>
-      <Router>
-        <Routes>
+      <Routes>
           <Route path='/' element={
             <ToolsHome {...props} />
           }/>
           <Route path='alt-text-helper' element={
-            <AltTextHome {...props}/>
-          }/>          
-        </Routes>
-      </Router>
+            <AltTextLayout globals={props.globals} />
+          }>
+            <Route index element={<AltTextHome {...props}/>} />
+            <Route path='review' element={<AltTextReview />} />
+          </Route>
+      </Routes>
     </div>
   );
 }
